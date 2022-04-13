0915ad5726c7571adb2defb4427210f8cb31e798
yournumberone <marsthegod@protonmail.com>
2022-04-13 10:38:45 +0300

    update navbar

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,5 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    @posts = Post.all
+  end
 end
--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -7,6 +7,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import '../js/bootstrap_js_files.js'
+import "@fortawesome/fontawesome-free/css/all"
 
 Rails.start()
 Turbolinks.start()
