542b3ab4b53d04d3d1defd1cc1c9aa0902eea544
yournumberone <marsthegod@protonmail.com>
2022-04-21 03:18:32 +0300

    more commits for actions

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
-  belongs_to :category, class_name: 'PostCategory', foreign_key: 'post_category_id'
+  belongs_to :post_category
   has_many :comments, class_name: 'PostComment', dependent: :destroy
   has_many :likes, class_name: 'PostLike', dependent: :destroy
 
